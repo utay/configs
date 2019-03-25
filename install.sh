3c6c1e96fa2c569f6b7c13e97c2c0438f3e90e29
Yannick Utard <yannick@frontapp.com>
2019-03-25 11:19:24 +0100

    Update script with pacman/i3 setup

--- install.sh
+++ install.sh
@@ -22,14 +22,67 @@ fi
 
 if [[ "$mode" = "full" || "$mode" = "f" ]]; then
     echo "--> Setup Linux configuration"
-    sudo apt-get update
-    sudo apt-get upgrade -y
-    sudo apt-get install -y curl git zsh vim golang python \
-        python-dev build-essential cmake gdb valgrind
 
-    echo "--> Install OhMyZsh"
-    git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+    # Make work directories
+    mkdir ~/go ~/Projects
+
+    # Install pacman packages
+    sudo pacman -Syu
+    sudo pacman -S $(cat pkglist.txt)
+
+    # Add current user to docker group
+    sudo usermod -aG docker $USER
+
+    # Install yaourt packages
+    yaourt -S $(cat yaourtlist.txt)
+
+    # Set zsh as default shell
     chsh -s $(grep /zsh$ /etc/shells | tail -1)
+
+    # Install prezto
+    git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+    zsh -c 'setopt EXTENDED_GLOB
+    for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+      ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+    done'
+
+    # Install go deps
+    go get -u $(cat godeps.txt)
+
+    # Install binaries
+
+    # Install i3lock fancy
+    cd ~/Projects
+    git clone https://github.com/meskarune/i3lock-fancy.git
+    cd i3lock-fancy
+    sudo make install
+
+    # kubectl
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+    chmod +X kubectl
+    sudo mv kubectl /usr/local/bin
+
+    # kubectx, kubens
+    sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+    sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+    sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+    # stern
+    mkdir -p $GOPATH/src/github.com/wercker
+    cd $GOPATH/src/github.com/wercker
+    git clone https://github.com/wercker/stern.git && cd stern
+    govendor sync
+    go install
+
+    # terraform
+    curl -LO https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
+    unzip terraform_0.11.13_linux_amd64.zip
+    sudo mv terraform /usr/local/bin
+    rm -f terraform_0.11.13_linux_amd64.zip
+
+    # TODO: install git aliases, zsh aliases
+    # TODO: symlink vimrc, .zshrc, .zprezto, josh theme, .gitconfig
+    # TODO: chrome default browser
 fi
 
 echo "--> Setup Vim configuration"
